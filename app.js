2bd3b281cbee765542cd5d7cbbf5e7c2715d8020
Linton Mackereth <linton@Lintons-MBP.home>
2017-08-30 19:16:11 +0100

    Initial commit with basic configuration

---
+++ app.js
@@ -0,0 +1,26 @@
+
+var path = require('path')
+
+require('dotenv').config({
+  path: path.join(__dirname, settings.env)
+})
+
+var mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useMongoClient: true
+})
+
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
+
+
+
