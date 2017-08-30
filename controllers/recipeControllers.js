12f7572f75b8c17ec444af82f4136949f7bf93ef
Linton Mackereth <linton@Lintons-MBP.home>
2017-08-30 20:07:46 +0100

    Handle saving of new recipes

--- controllers/recipeControllers.js
+++ controllers/recipeControllers.js
@@ -2,8 +2,18 @@ var Recipe = require('../models/Recipe')
 
 function recipeControllers (app) {
   app.post('/recipes', function (req, res) {
-    res.send({
-      hello: 'world'
+    var recipe = Recipe(req.body)
+
+    recipe.save(function(error, savedRecipe){
+      if (error) {
+        res.send({
+          error: 'Unable to perform request. An error occurred.'
+        })
+
+        return
+      }
+
+      res.send({_id: savedRecipe._id})
     })
   })
 }
