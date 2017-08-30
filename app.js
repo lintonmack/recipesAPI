46cd5887817268332b204b7d5aeb85fa700cd42a
Linton Mackereth <linton@Lintons-MBP.home>
2017-08-30 19:33:17 +0100

    Added function for recipe controllers

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+require('./controllers/recipeControllers')(app) 
+
 app.listen(3000, function() {
   console.log('Listening on port 3000')
 })
---
+++ controllers/recipeControllers.js
@@ -0,0 +1,5 @@
+var Recipe = require('../models/Recipe')
+
+function recipeControllers (app) {
+  module.exports = recipeControllers
+}
